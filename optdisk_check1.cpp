4c61cddf0d62362882351ba3c02ffb7d43dc5e64
Minh Duc Vo <32769878+minhducsun2002@users.noreply.github.com>
2018-05-26 20:49:43 +0700

    Add moving / swapping logic

--- optdisk_check1.cpp
+++ optdisk_check1.cpp
@@ -44,21 +44,41 @@ int main()
 
     // input processing done!
 
-    for (llint i = 1 ; i <= capacity ; i++)
-    cout << disk[i] << " " << ((i % 10 == 0) ? "\n" : ""); cout << endl << endl << endl;
+    for (llint n = 1 ; n <= capacity ; n++)
+    cout << ((n % 10 == 1) ? to_string(n) : string ()) + " " << disk[n] << " " << ((n % 10 == 0) ? "\n" : ""); cout << endl;
 
     // now checking answer
 
     //llint timing = 0;
     char directive; llint arg1, arg2, arg3;
-    //while 
-    (cin >> directive >> arg1 >> arg2 >> arg3);
-    
+    llint iteration; cin >> iteration;
+    for (llint i = 1 ; i <= iteration ; i++)
+    {
+    	cin >> directive >> arg1 >> arg2 >> arg3;
+        if (directive == copy_directive)
+        {
+        	vector <file_id> buffer;
+	        for (llint i = 1 ; i <= arg3 ; i++)
+	        {
+	            buffer.push_back(disk[arg1 + i - 1]);
+	            disk[arg1 + i - 1] = 0;
+	        }
+	        for (llint i = 1 ; i <= arg3 ; i++) disk[arg2 + i - 1] = buffer[i - 1];
+        }
+    	if (directive == swap_directive)
+    	{
+    		vector <file_id> buffer;
+    		for (llint i = 1 ; i <= arg3 ; i++) buffer.push_back(disk[arg1 + i - 1]);
+    		for (llint i = 1 ; i <= arg3 ; i++) swap(buffer[i - 1], disk[arg2 + i - 1]);
+    		for (llint i = 1 ; i <= arg3 ; i++) disk[arg1 + i - 1] = buffer[i - 1];
+    	}
+    	cout << "Iteration " << i << endl;
+    	for (llint n = 1 ; n <= capacity ; n++)
+    	cout << ((n % 10 == 1) ? to_string(n) : string ()) + " " << disk[n] << " " << ((n % 10 == 0) ? "\n" : ""); cout << endl;
+    }
 
-    vector <file_id> buffer;
-    for (llint i = 1 ; i <= arg3 ; i++) buffer.push_back(disk[arg1 + i - 1]);
-    for (llint i = 1 ; i <= arg3 ; i++) disk[arg2 + i - 1] = buffer[i - 1];
+    // for (llint i = 1 ; i <= capacity ; i++)
+    // cout << disk[i] << " " << ((i % 10 == 0) ? "\n" : ""); cout << endl;
 
-    for (llint i = 1 ; i <= capacity ; i++)
-    cout << disk[i] << " " << ((i % 10 == 0) ? "\n" : ""); cout << endl;
+
 }
